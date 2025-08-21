07edb603d4e62e26afc9de431f2f1f32cbff47f1
Your Full Name <ehasun0909@gamil.com>
2025-08-21 15:48:00 +0600

    realize event loop

---
+++ Unleashing-the-Power-of-Node.js/module_1/eventLoop.js
@@ -0,0 +1,15 @@
+console.log("1");
+
+setTimeout(() => {
+  console.log("2");
+
+  setTimeout(() => {
+    console.log("3");
+  }, 0);
+}, 0);
+
+setTimeout(() => {
+  console.log("4");
+}, 0);
+
+console.log("5");
